28f446c2f5d7e400501c39352f3ca5ab374653fc
Sidharth Narayanan <iamisidharth@gmail.com>
2019-01-23 14:03:58 +0530

    updated workflow

--- main.py
+++ main.py
@@ -1,11 +1,9 @@
 #!/usr/bin/env python3
 import sys
 import pprint
-from parser import Action
-# from parser import PDDL_Parser
+
 from pddl_parser import PDDL_Parser
-# from planner import Astar_planner
-# from Astar import Astar
+
 from planner import *
 
 if __name__ == "__main__":
